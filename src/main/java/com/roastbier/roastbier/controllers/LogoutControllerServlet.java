a45b70dfa00a2c3ed7b3ea51698e019872319948
Matheus Costa <matheuscostakrenn@gmail.com>
2023-07-19 12:47:21 -0300

    Criado rota logout

---
+++ src/main/java/com/roastbier/roastbier/controllers/LogoutControllerServlet.java
@@ -0,0 +1,20 @@
+package com.roastbier.roastbier.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutControllerServlet", value = "/logout")
+public class LogoutControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect(System.getProperty("BASE_URL"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
