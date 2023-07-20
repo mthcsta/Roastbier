de565298e669ef6bb9a8c4f23e5edcbbc0db22e8
Matheus Costa <matheuscostakrenn@gmail.com>
2023-07-19 23:05:03 -0300

    Criado list

---
+++ src/main/webapp/src/scripts/list/global.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    $("#btn-insert").on("click", function() {
+        window.location.href = $("#btn-update").attr("href") + '&id=' + $(".select_row:checked").val();
+    });
+
+    const updateUrl = $("#btn-update").attr("href");
+    $("#btn-update").attr("href", "javascript:alert('Você precisa selecionar um registro');");
+
+    $("#btn-update").on("click", function(){
+        if ($(".select_row:checked").length == 0) {
+            alert('Você precisa selecionar um registro')
+            return;
+        }
+        if ($(".select_row:checked").length > 1) {
+            alert('Você só pode selecionar 1 registro')
+            return;
+        }
+
+        window.location.href = $("#btn-update").attr("href") + '&id=' + $(".select_row:checked").val();
+    });
+
+    $("#btn-delete").on("click", function() {
+        $(".select_row").serializeArray();
+    });
+
+    $("#btn-filter").on('click', function() {
+        const filterValue = $("#filter-value").val();
+        const filterModel = $(this).data("model");
+        $.ajax({
+            url: url_path("/ajax/list?m=" + filterModel),
+            type: "GET",
+            dataType:"json",
+            data: { search: filterValue },
+            success: function(data) {
+                $("#table_search").trigger('fill-table', {data});
+            }
+        });
+    });
+
+});
---
+++ src/main/webapp/src/scripts/list/product.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    const datatable = $("#table_search").DataTable({
+        columns: [
+            {
+                title: 'Selecionar',
+                render: function(data, type, row) {
+                    return `<input type="checkbox" name="select[]" class="select_row" value="${row.cpf}" />`;
+                },
+                width: 30,
+                orderable: false,
+                class: "text-center"
+            },
+            { title: 'Name', data: 'nome' },
+            { title: 'Unity', data: 'unidade' },
+            { title: 'Unitary Price', data: 'precoUnitario' },
+            { title: 'Description', data: 'descricao' },
+        ],
+        columnDefs: [{ targets: 0, orderable: false}],
+        order: [[ 2, 'asc' ]],
+        data: [],
+        searching: false,
+        responsive: true,
+        scrollCollapse: true,
+        deferRender: true,
+    });
+
+    $("#table_search").on('fill-table', function (event, obj) {
+        datatable.clear().rows.add(obj.data).draw(false);
+    });
+
+    $("#btn-filter").trigger('click');
+
+});
